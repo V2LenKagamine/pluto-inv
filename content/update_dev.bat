77626f89c9efc8906d2d3d95e15091ce9b819d7b
Len Kagamine <xboxmaster2000@hotmail.com>
2025-04-24 02:51:59 -0400

    Stupid Icon Thing

--- content/update_dev.bat
+++ content/update_dev.bat
@@ -1,4 +1,4 @@
-"F:\Steam\steamapps\common\GarrysMod\bin\gmpublish.exe" update -id 3463799131 -addon pack0.gma
-"F:\Steam\steamapps\common\GarrysMod\bin\gmpublish.exe" update -id 3463800529 -addon pack1.gma
-"F:\Steam\steamapps\common\GarrysMod\bin\gmpublish.exe" update -id 3463801989 -addon pack2.gma
+"F:\Steam\steamapps\common\GarrysMod\bin\gmpublish.exe" update -id 3463799131 -addon pack0.gma -icon icon.jpg
+"F:\Steam\steamapps\common\GarrysMod\bin\gmpublish.exe" update -id 3463800529 -addon pack1.gma -icon icon.jpg
+"F:\Steam\steamapps\common\GarrysMod\bin\gmpublish.exe" update -id 3463801989 -addon pack2.gma -icon icon.jpg
 pause
