06f7d7be29e9aed3ad1f6f17db18078356fa24de
Len Kagamine <xboxmaster2000@hotmail.com>
2026-07-31 02:35:04 -0400

    Temp disable tickets
    AAAAAAAA

--- lua/pluto/events/sv_init.lua
+++ lua/pluto/events/sv_init.lua
@@ -346,6 +346,11 @@ function pluto.inv.readqueueevent(requester)
         return
     end
 
+    if(true) then
+        requester:ChatPrint(Color(200, 50, 50),"Tickets are currently disabled T_T")
+        return 
+    end
+
     pluto.db.transact(function(db) 
 		if (not pluto.inv.addcurrency(db, requester, "ticket", price)) then
 			mysql_rollback(db)
